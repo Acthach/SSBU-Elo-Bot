0811f1400f6825797b1ad3ff546941dd081e8e23
Acthach <arthur.thach@gmail.com>
2025-02-13 13:02:30 -0800

    Add Python file

---
+++ elo.py
@@ -0,0 +1,20 @@
+class Implementation:
+    """
+    A class that represents an implementation of the Elo Rating System
+    """
+
+    def __init__(self, base_rating=1000):
+        """
+        Runs at initialization of class object.
+        @param base_rating - The rating a new player would have
+        """
+        self.base_rating = base_rating
+        self.players = []
+
+    #get player list
+    #get specific player
+    #addplayer
+    #remove player
+    #record match
+    #get player rating
+    #get rating list
