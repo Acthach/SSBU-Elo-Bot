20c6b9565dc8eb07d193647c370cbafc51f0c250
Acthach <arthur.thach@gmail.com>
2025-02-16 21:49:09 -0800

    first test

--- register_commands.py
+++ register_commands.py
@@ -1,6 +1,24 @@
+import discord
+from discord.ext import commands
+
+"""
 import requests
 import yaml
 
-TOKEN =''
-Application_ID = ''
+TOKEN ='MTM0MDUzMzI5OTQ1MzQ5NzM5NA.GlS_fa.BYJFUbPcxIZwqztyuh6GRldURsb1pvYyx67JkE'
+Channel_ID = '1340551594697297930'
 URL = f''
+"""
+
+TOKEN ='MTM0MDUzMzI5OTQ1MzQ5NzM5NA.GlS_fa.BYJFUbPcxIZwqztyuh6GRldURsb1pvYyx67JkE'
+Channel_ID = '1340551594697297930'
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("testing")
+    channel = bot.get_channel(Channel_ID)
+    await channel.send("testing")
+
+bot.run(TOKEN)
