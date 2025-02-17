0a1af47c61d87d8605f48fd95d0d1ce3ad24cdb6
Acthach <arthur.thach@gmail.com>
2025-02-16 22:19:03 -0800

    first test

--- register_commands.py
+++ register_commands.py
@@ -1,24 +1,27 @@
 import discord
 from discord.ext import commands
-
-"""
-import requests
-import yaml
-
 TOKEN ='MTM0MDUzMzI5OTQ1MzQ5NzM5NA.GlS_fa.BYJFUbPcxIZwqztyuh6GRldURsb1pvYyx67JkE'
 Channel_ID = '1340551594697297930'
-URL = f''
-"""
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
 
-TOKEN ='MTM0MDUzMzI5OTQ1MzQ5NzM5NA.GlS_fa.BYJFUbPcxIZwqztyuh6GRldURsb1pvYyx67JkE'
-Channel_ID = '1340551594697297930'
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print("testing")
     channel = bot.get_channel(Channel_ID)
-    await channel.send("testing")
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
 
 bot.run(TOKEN)
